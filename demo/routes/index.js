54c248e14afc391856e831b16e1a1131ee869df6
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-22 13:42:58 +0200

    Dockerfile

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const os = require('os');
 
 /* GET home page. */
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express', 
+    runtime: `${os.platform()} ${os.release()}`,
+    
+  });
 });
 
 module.exports = router;
