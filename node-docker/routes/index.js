ed95b357ccbb0c11cc2a024486e0aca64664d7e3
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-22 13:47:35 +0200

    Show platform

--- node-docker/routes/index.js
+++ node-docker/routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var os = require('os');
 var router = express.Router();
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    runtime: `${os.platform()} ${os.release()}`,
+  });
 });
 
 module.exports = router;
